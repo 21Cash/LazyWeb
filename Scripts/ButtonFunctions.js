8e870ca981eecdb2146f5e7295c240a2b0d0259c
21ThousandProof <21ThousandProof.@gmail.com>
2022-12-03 14:48:31 +0530

    Added Additional Stats

--- Scripts/ButtonFunctions.js
+++ Scripts/ButtonFunctions.js
@@ -1,6 +1,10 @@
 //This JS File Contains All On click Functions For Buttons
 
 
+const minAttendance = 75;
+const classesPerDay = 7;
+
+
 function OnClick_ShowStats() {
 	
 	var statsElement = document.getElementById("stats");
@@ -12,24 +16,107 @@ function OnClick_ShowStats() {
 	
 	var str = "Classes Attended : " + stats.classesAttended + "<br>"
 				+ "Total Classe : " + stats.totalClasses + "<br>"
-				+ "Attendance percentage : " + stats.percentage.toFixed(2) + "%";
-	
+				+ "Attendance percentage : " + stats.percentage.toFixed(2) + "% <br>"
+				+ "Continuous Leaves : " + stats.continuousClassLeaves + " classes or " + stats.continuousDayLeaves + " days<br>" 
+				+ "To Gain 75% Attendance : " + stats.continuousClassesToPresent + " classes or " + stats.continuousDaysToPresent + " days";
 	statsElement.innerHTML = str;
 	
 }
 
 
+
+
 function GetStats(_classesAttended, _totalClasses) {
 	
-	var _percentage = (_classesAttended / _totalClasses) * 100;
-	
+	var _percentage = GetPercentage(_classesAttended, _totalClasses);
+	var conLeaveStats = GetContinuousLeaves(_classesAttended, _totalClasses);
+	var conPresentStats = GetClassesToBePresent(_classesAttended, _totalClasses);
 	var stats =  {
 		classesAttended : _classesAttended,
 		totalClasses : _totalClasses,
+		continuousClassLeaves : conLeaveStats.classes,
+		continuousDayLeaves : conLeaveStats.days,
+		continuousClassesToPresent : conPresentStats.classes,
+		continuousDaysToPresent : conPresentStats.days,
 		percentage : _percentage
 	};
 	
 	return stats;
 }
 
+function GetPercentage(classesAttended, totalClasses) {
+	
+	return (classesAttended / totalClasses) * 100;
+	
+}
 
+
+
+// This Function Return the Continous Class Leaves And Day Leaves Which can be taken
+function GetContinuousLeaves(_present, _total) {
+	
+	var _p = _present;
+	var _t = _total;
+	
+	var tAttendance  = GetPercentage(_p, _t);
+	
+	var cnt = 0;
+	
+	while(tAttendance > minAttendance) {
+		_t++;
+		tAttendance = GetPercentage(_p, _t);
+		cnt++;
+	}
+	
+	var leaveDays = cnt / classesPerDay;
+	
+	var leaveStats = {
+		classes : cnt,
+		days : leaveDays.toFixed(2)	
+	};
+	
+	return leaveStats;
+}
+
+
+// pi GetNumberOfClassesToPresent(float _present, float _total) {
+	
+// 	float _percentage = GetPercentage(_present, _total);	
+	
+// 	int classCnt = 0;
+	
+// 	while(_percentage < minAttendance) {
+// 		_present++;
+// 		_total++;
+		
+// 		_percentage = GetPercentage(_present, _total);
+// 		classCnt++;
+// 	}
+	
+// 	int daysCnt = classCnt / classCountPerDay;
+	
+// 	return MP(classCnt, daysCnt);
+// }
+
+function GetClassesToBePresent(_present, _total) {
+	
+	var _t = GetPercentage(_present, _total);
+	
+	var cnt = 0;
+	
+	while(_t < minAttendance) {
+		_present++;
+		_total++;
+		
+		_t = GetPercentage(_present, _total);
+		cnt++;
+	}
+	
+	var leaveDays = cnt / classesPerDay;
+	
+	var leaveStats = {
+		classes : cnt,
+		days : leaveDays.toFixed(2)	
+	};
+	return leaveStats;
+}
