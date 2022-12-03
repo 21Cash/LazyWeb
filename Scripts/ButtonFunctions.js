5209d9b971b4771f093df0173c1bba9dc786b046
21ThousandProof <21ThousandProof.@gmail.com>
2022-12-03 14:21:35 +0530

    Added Attendance Calculator

---
+++ Scripts/ButtonFunctions.js
@@ -0,0 +1,35 @@
+//This JS File Contains All On click Functions For Buttons
+
+
+function OnClick_ShowStats() {
+	
+	var statsElement = document.getElementById("stats");
+	
+	var f = document.getElementById("classesAttended").value;
+	var s = document.getElementById("totalClasses").value;
+	
+	stats = GetStats(f, s);
+	
+	var str = "Classes Attended : " + stats.classesAttended + "<br>"
+				+ "Total Classe : " + stats.totalClasses + "<br>"
+				+ "Attendance percentage : " + stats.percentage;
+	
+	statsElement.innerHTML = str;
+	
+}
+
+
+function GetStats(_classesAttended, _totalClasses) {
+	
+	var _percentage = (_classesAttended / _totalClasses) * 100;
+	
+	var stats =  {
+		classesAttended : _classesAttended,
+		totalClasses : _totalClasses,
+		percentage : _percentage
+	};
+	
+	return stats;
+}
+
+
